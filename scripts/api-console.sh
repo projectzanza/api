ed4e261ce9c30b33c8860032d25e56a719deb7a1
Mark O'Shea <mark@osheatech.com>
2017-05-31 22:57:15 +0100

    add rocket chat docker containers to the API docker-compose file

--- scripts/api-console.sh
+++ scripts/api-console.sh
@@ -2,5 +2,5 @@
 # when the api console launches, run startup-dev.sh to start the rails app
 # use start-debug.sh to run the debugging instance from RubyMine
 
-docker-compose up -d db mail_server
+docker-compose up -d db mail_server mongo-init-replica rocketchat
 docker-compose run --service-ports api sh
